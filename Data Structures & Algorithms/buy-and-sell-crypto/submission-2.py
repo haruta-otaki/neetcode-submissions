4db4eb7673c73e7af2834e71b3116bd57bee8229
Haruta Otaki <haotaki@davidson.edu>
2026-07-03 15:23:32 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        if (n < 2):
+            return 0
+
+        max_profit = 0 
+        profits = [0] * n
+        lowest_buy = prices[0]
+        for i in range(1, n):
+            if (prices[i] < lowest_buy): 
+                lowest_buy = prices[i]
+            profits[i] = prices[i] - lowest_buy
+            if (profits[i] > max_profit): 
+                max_profit = profits[i] 
+        return max(profits)
