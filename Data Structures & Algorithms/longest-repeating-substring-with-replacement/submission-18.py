f65ea6f30223dc7b152c46aa45086f3cd19f0678
Haruta Otaki <haotaki@davidson.edu>
2026-07-05 16:45:34 -0400

    Add: longest-repeating-substring-with-replacement - submission-18

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-18.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        r = 0
+        output = 0
+        while (r <= len(s)):
+            sub_s = s[l:r]
+            f = 0
+            d = {}
+            # print(sub_s)
+            for j in range(len(sub_s)): 
+                if d.get(sub_s[j]): 
+                    d[sub_s[j]] += 1
+                else: 
+                    d[sub_s[j]] = 1
+                # print(sub_s[j], d[sub_s[j]])
+                f = max(f, d[sub_s[j]])
+            if (len(sub_s) - f) <= k: 
+                output = max(output, len(sub_s))
+                # print(output)
+                r += 1
+            else: 
+                l += 1
+
+        return output
