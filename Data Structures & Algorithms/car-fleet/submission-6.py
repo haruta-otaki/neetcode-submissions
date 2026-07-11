7030df99c670f48bfc0d14c03c531ec9aea9f6e7
Haruta Otaki <haotaki@davidson.edu>
2026-07-11 16:09:56 -0400

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        d = {}
+        output = 0
+        for i in range(n): 
+            d[(position[i])] = (target-position[i]) / speed[i]
+        keys = sorted(list(d.keys()))
+
+        for i in range(n - 1, -1, -1): 
+            if d.get(keys[i]): 
+                key = keys[i]
+                for j in range(i-1, -1, -1): 
+                    if (d.get(keys[j]) and d.get(key)):
+                        if (d[keys[j]] <= d[key]): 
+                            d.pop(keys[j])
+        return len(d)
+
+
+
+            
+
+         
